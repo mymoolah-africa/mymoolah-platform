bca42a41243734acc2c3551f1c41a2fb23e453b2
Andre Botes <YOUR_GITHUB_EMAIL@example.com>
2025-11-19 22:56:39 +0200

    Fix VAT calculation for fixed fees: stored values are VAT-exclusive, don't divide

--- services/tierFeeService.js
+++ services/tierFeeService.js
@@ -137,11 +137,11 @@ async function calculateTierFees(userId, supplierCode, serviceType, transactionA
     
     switch (config.supplier_fee_type) {
       case 'fixed':
+        // Stored fixed fee is always VAT-exclusive (net base amount)
         supplierCostExclVatCents = parseInt(config.supplier_fixed_fee_cents) || 0;
-        // If stored as inclusive, extract exclusive amount
-        if (supplierVatInclusive) {
-          supplierCostExclVatCents = Math.round(supplierCostExclVatCents / (1 + supplierVatRate));
-        }
+        // supplier_vat_inclusive flag means "supplier charges VAT-inclusive", not "stored value is inclusive"
+        // Since stored value is already exclusive, we don't need to divide
+        // VAT will be added in the next step to get the inclusive amount
         break;
         
       case 'percentage':
@@ -177,11 +177,11 @@ async function calculateTierFees(userId, supplierCode, serviceType, transactionA
     
     switch (config.mm_fee_type) {
       case 'fixed':
+        // Stored fixed fee is always VAT-exclusive (net base amount)
         mmFeeExclVatCents = parseInt(config.mm_fixed_fee_cents) || 0;
-        // If stored as inclusive, extract exclusive amount
-        if (mmVatInclusive) {
-          mmFeeExclVatCents = Math.round(mmFeeExclVatCents / (1 + mmVatRate));
-        }
+        // mm_vat_inclusive flag means "user pays VAT-inclusive", not "stored value is inclusive"
+        // Since stored value is already exclusive, we don't need to divide
+        // VAT will be added in the next step to get the inclusive amount
         break;
         
       case 'percentage':
